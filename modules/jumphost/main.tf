1752fbf972289d0c35b1e3860212da1feb92a48a
demid.krom <demid.krom@l-p-a.com>
2026-08-17 14:53:06 +0200

    fix(jumphost): changed to standard static public ip and bumped azurerm provider version

--- modules/jumphost/main.tf
+++ modules/jumphost/main.tf
@@ -2,7 +2,8 @@ resource "azurerm_public_ip" "jumphost_pip" {
   name                = "pip-jumphost"
   resource_group_name = var.resource_group_name
   location            = var.location
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_network_interface" "jumphost_nic" {
